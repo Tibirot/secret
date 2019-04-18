7f3558ccb5c930d389214f8084ce2b988bc7b017
Socea Tiberiu <soceatiberiuv@gmail.com>
2019-04-18 21:00:38 +0300

    Added env files

---
+++ app.js
@@ -0,0 +1,103 @@
+//jshint esversion:8
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true}, (error) => {
+  if (!error) {
+    console.log("connected successfully");
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+const User = new mongoose.model("User", userSchema);
+
+/////////////////////////////// HOME ROUTE//////////////////////////////////////////////////
+
+app.route('/')
+
+  .get((req, res) => {
+    res.render("home");
+  });
+
+/////////////////////////////// LOGIN ROUTE//////////////////////////////////////////////////
+
+app.route('/login')
+
+  .get((req, res) => {
+    res.render("login");
+  })
+
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser) => {
+      if (err) {
+        console.log(err);
+      }else {
+        if(foundUser){
+          if (foundUser.password === password) {
+            res.render("secrets");
+          }
+        }
+      }
+    });
+  });
+
+/////////////////////////////// REGISTER ROUTE////////////////////////////////////////////////
+
+app.route('/register')
+
+  .get((req, res) => {
+    res.render("register");
+  })
+
+  .post((req, res) =>{
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save((err) => {
+      if (err) {
+        console.log(err);
+      }else {
+        res.render("secrets");
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log("Server started on port: " + port);
+});
