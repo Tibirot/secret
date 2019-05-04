b41ad4d331ca6e3d19d42c0c153af8e3cb5899ef
Socea Tiberiu <soceatiberiuv@gmail.com>
2019-05-04 21:02:59 +0300

    added passport and session

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,21 +16,38 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: "Our big secret.",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true}, (error) => {
   if (!error) {
     console.log("connected successfully");
   }
 });
 
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
-/////////////////////////////// HOME ROUTE//////////////////////////////////////////////////
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+/////////////////////////////// HOME ROUTE//////////////////////////////////////
 
 app.route('/')
 
@@ -37,7 +55,7 @@ app.route('/')
     res.render("home");
   });
 
-/////////////////////////////// LOGIN ROUTE//////////////////////////////////////////////////
+/////////////////////////////// LOGIN ROUTE/////////////////////////////////////
 
 app.route('/login')
 
@@ -46,25 +64,24 @@ app.route('/login')
   })
 
   .post((req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email: username}, (err, foundUser) => {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    });
+
+    req.login(user, (err) => {
       if (err) {
         console.log(err);
-      }else {
-        if(foundUser){
-            bcrypt.compare(password, foundUser.password, (err, result) => {
-              if (result === true) {
-                res.render("secrets");
-              }
-            });
-        }
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
       }
     });
   });
 
-/////////////////////////////// REGISTER ROUTE////////////////////////////////////////////////
+/////////////////////////////// REGISTER ROUTE//////////////////////////////////
 
 app.route('/register')
 
@@ -73,26 +90,40 @@ app.route('/register')
   })
 
   .post((req, res) =>{
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
 
-      const newUser = new User({
-        email: req.body.username,
-        password: hash
-      });
-
-      newUser.save((err) => {
-        if (err) {
-          console.log(err);
-        }else {
-          res.render("secrets");
-        }
-      });
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
     });
+
   });
 
+/////////////////////////////// SECRETS ROUTE//////////////////////////////////
 
+app.route('/secrets')
 
+  .get((req, res) => {
+    if (req.isAuthenticated) {
+      res.render("secrets");
+    } else {
+      res.redirect("/login");
+    }
+  });
 
+/////////////////////////////// LOGOUT ROUTE//////////////////////////////////
+
+app.route("/logout")
+
+  .get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
 
 let port = process.env.PORT;
 if (port == null || port == ""){
