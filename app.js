ce9876e605e3b409c397f7331f3c35630fdd1aa9
Socea Tiberiu <soceatiberiuv@gmail.com>
2019-05-05 18:29:19 +0300

    Added OAuth

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
@@ -39,13 +41,37 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://googleapis.com/oauth2/v3/userinfo",
+    scope: ["profile"]
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 /////////////////////////////// HOME ROUTE//////////////////////////////////////
 
@@ -53,6 +79,19 @@ app.route('/')
 
   .get((req, res) => {
     res.render("home");
+  });
+
+/////////////////////////////// GOOGLE AUTH ROUTE///////////////////////////////
+
+app.route('/auth/google')
+  .get(passport.authenticate("google", { scope: ["profile"] }));
+
+/////////////////////////////// GOOGLE SECRETS ROUTE///////////////////////////////
+
+app.route("/auth/google/secrets")
+  .get(passport.authenticate("google", { failureRedirect: "/login"}),
+  (req, res) => {
+    res.redirect("/secrets");
   });
 
 /////////////////////////////// LOGIN ROUTE/////////////////////////////////////
